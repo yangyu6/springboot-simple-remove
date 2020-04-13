a45ea67c04bc8afab1d6f201c09fd3a32b8fb62d
yu <773024110@qq.com>
2020-04-13 16:51:10 +0800

    增加redis配置

--- src/test/java/com/yu/food_review/SpringBootApplicationTests.java
+++ src/test/java/com/yu/food_review/SpringBootApplicationTests.java
@@ -1,7 +1,9 @@
 package com.yu.food_review;
 
+import com.yu.common.utils.RedisUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,11 +11,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringBootApplicationTests {
 
-
+    @Autowired
+    private RedisUtil redisUtil;
 
     @Test
-    public void contextLoads()  {
-
+    public void contextLoads() {
+        redisUtil.set("yu的key", "我是一个value超时时间为10s", 10);
     }
 
 }
